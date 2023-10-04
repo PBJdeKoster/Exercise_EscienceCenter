8e5c35458712e209fe3277640ed2e4c1434c904e
Pascal de Koster <pascal.dekoster@tno.nl>
2023-10-04 15:37:39 +0200

    added a function add and subtract

---
+++ example.py
@@ -0,0 +1,13 @@
+import pytest
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a + b  # do not change this line until prompted to do so.
+
+def test_add():
+	assert add(1,2) == 3
+
+# def test_subtract():
+# 	assert subtract(1,2) == -1
